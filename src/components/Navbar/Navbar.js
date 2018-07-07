eb19752a474e5bcea7d491556ee93c14c216dc34
Leo Taylor <leotaylor723@gmail.com>
2018-07-07 14:03:57 -0500

    can navigate around page while logged in

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 
+import authRequests from '../../firebaseRequests/auth';
+
 import './Navbar.css';
 
 class Navbar extends React.Component {
   render () {
+    const {authed, runAway} = this.props;
+    const logoutClickEvent = () => {
+      authRequests.logoutUser();
+      runAway();
+    };
+
     return (
       <div className="Navbar">
         <nav className="navbar navbar-inverse">
@@ -19,11 +27,32 @@ class Navbar extends React.Component {
               <Link to="/" className="navbar-brand">Fish Store</Link>
             </div>
             <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-              <ul className="nav navbar-nav navbar-right">
-                <li>
-                  <Link to="/login">Login</Link>
-                </li>
-              </ul>
+              {
+                authed ? (
+                  <ul className="nav navbar-nav navbar-right">
+                    <li>
+                      <Link to="/inventory">Inventory</Link>
+                    </li>
+                    <li>
+                      <Link to="/orders">Orders</Link>
+                    </li>
+                    <li className="navbar-form">
+                      <button
+                        onClick={logoutClickEvent}
+                        className="btn btn-danger"
+                      >
+                        Logout
+                      </button>
+                    </li>
+                  </ul>
+                ) : (
+                  <ul className="nav navbar-nav navbar-right">
+                    <li>
+                      <Link to="/login">Login</Link>
+                    </li>
+                  </ul>
+                )
+              }
             </div>
           </div>
         </nav>
