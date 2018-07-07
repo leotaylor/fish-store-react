a26baebe9c073f71e4bc0b6ab8babab90a3f73b0
Leo Taylor <leotaylor723@gmail.com>
2018-07-07 09:42:30 -0500

    made 10 components

---
+++ src/components/Fish/Fish.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Fish.css';
+
+class Fish extends React.Component {
+  render () {
+    return (
+      <h1 className="Fish">Fish</h1>
+    );
+  }
+}
+
+export default Fish;
---
+++ src/components/SingleOrder/SingleOrder.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './SingleOrder.css';
+
+class SingleOrder extends React.Component {
+  render () {
+    return (
+      <h1 className="SingleOrder">SingleOrder</h1>
+    );
+  }
+}
+
+export default SingleOrder;
