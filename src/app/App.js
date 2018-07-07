56ade96627b997e0b1fc2101c8d4aa988dfccf15
Leo Taylor <leotaylor723@gmail.com>
2018-07-07 12:43:08 -0500

    form added and email password events

--- src/app/App.js
+++ src/app/App.js
@@ -14,6 +14,9 @@ import Navbar from '../components/Navbar/Navbar';
 import Register from '../components/Register/Register';
 // import SingleOrder from '../components/SingleOrder/SingleOrder';
 
+import fbConnection from '../firebaseRequests/connection';
+fbConnection();
+
 const PrivateRoute = ({ component: Component, authed, ...rest}) => {
   return (
     <Route
